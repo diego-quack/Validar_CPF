e42432ce7c380bb942a6e2ecc62bfc3f773b6877
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-02-12 22:25:42 -0300

    Inicio da criação da classe CPF: construtor e método de calculo do primeiro dígito em andamento.

--- src/entities/CPF.java
+++ src/entities/CPF.java
@@ -1,5 +1,34 @@
 package entities;
 
+import java.util.Scanner;
+
 public class CPF {
+
+	private Scanner input = new Scanner(System.in);
+	private String cpf;
+	private String cpfTeste;
+	private int soma = 0;
+	private int digitoUm = 0;
+	private int digitoDois = 0;
+
+	public CPF() {
+		System.out.println("Digite seu CPF(apenas números): ");
+		cpf = input.nextLine();
+	}
+
+	public void calculoDigitoUm() {
+
+		soma = 0;
+		int i = 0;
+		int j = 10;
+
+		while (i < 9) {
+			soma += (Integer.parseInt((cpf.charAt(i) + "")) * j);
+
+			i++;
+			j--;
+		}
+
+	}
 
 }
